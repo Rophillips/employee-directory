8775fd664044b24c1066135deb942f535508df80
Roseanne Phillips <rphillips.rp@gmail.com>
2021-04-29 17:48:49 -0400

    trying to get search working

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import API from "./utils/API.js";
 import EmployeeTable from "./components/EmployeeTable";
 import SearchBar from "./components/SearchBar";
+import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
 
@@ -20,13 +21,37 @@ class App extends Component {
     // .then((response) => response.json())
     API.getUsers()
     .then((response) => {
-      console.log(response);
+      console.log(response.data.results);
       this.setState({
         items:response.data.results,
         loading:true
       })
     })
   }
+
+  handleRemove = id => {
+    // console.log(this.state);
+    this.setState({
+      employees: this.state.employees.filter(employee=> employee.id !== id)
+    });
+  }
+
+  handleInputChange = event => {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value
+    });
+  };
+
+    // When the form is submitted, search the Giphy API for `this.state.search`
+    handleFormSubmit = event => {
+      event.preventDefault();
+      this.getUsers(this.state.search);
+    };
+
+
+
   render() {
 
     //let {items, loading} = this.state
--- employee-directory/src/components/EmployeeTable.js
+++ employee-directory/src/components/EmployeeTable.js
@@ -7,7 +7,6 @@ function EmployeeTable(props) {
   <thead>
     <tr>
       {/* <th img="col">picture</th> */}
-      
       <th scope="col">First</th>
       <th scope="col">Last</th>
       <th scope="col">email</th>
@@ -17,11 +16,13 @@ function EmployeeTable(props) {
   <tbody>
       {props.employees.map(employee => (
           <tr>
+              
               {/* <td>{employee.picture.thumbnail}</td> */}
               <td>{employee.name.first}</td>
               <td>{employee.name.last}</td>
               <td>{employee.email}</td>
               <td>{employee.cell}</td>
+              {/* <td>{employee.id}</td> */}
           </tr>
       ))}
  
